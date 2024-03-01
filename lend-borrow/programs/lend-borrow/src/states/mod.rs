0962e38b319c982d74f7805a84efc137947a7778
Suvineet Singh <suvineet1998@gmail.com>
2024-03-01 22:50:55 +0000

    fix multiple import

--- lend-borrow/programs/lend-borrow/src/states/mod.rs
+++ lend-borrow/programs/lend-borrow/src/states/mod.rs
@@ -1,11 +1,9 @@
 pub mod active_loan;
 pub mod collection_pool;
-pub mod collection_pool;
 pub mod offer;
 pub mod vault;
 
 pub use active_loan::*;
-pub use collection_pool::*;
 pub use collection_pool::*;
 pub use offer::*;
 pub use vault::*;
