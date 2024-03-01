5537d67df067b7a36a89b94d6bf49678fc4bf0d7
Suvineet Singh <suvineet1998@gmail.com>
2024-03-01 22:57:55 +0000

    Add a create_pool instruction and call it from the lend_borrow module

---
+++ lend-borrow/programs/lend-borrow/src/instructions/create_pool.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+
+use crate::states::CollectionPool;
+
+#[derive(Accounts)]
+#[instruction(collection_id: Pubkey)]
+pub struct CreatePool<'info> {
+    #[account(
+        init, // initialise the collection pool account
+        /// we need only one collection pool to be created per NFT collection,
+        /// that’s why we add collection_id in the seeds, so that we can link this
+        /// collection pool account to a particular NFT collection, since a
+        /// specific combination of seeds will always derive the same program address.
+        seeds=[b"collection-pool", collection_id.key().as_ref()],
+        bump,
+        payer=authority, // account responsible to pay rent to store the account on chain
+        space=CollectionPool::LEN // amount of bytes required to store the account.
+    )]
+
+    /// Use a Box to store data on the heap rather than the stack, else,
+    /// we will get a stack overflow error if the amount of data becomes too big.
+    pub collection_pool: Box<Account<'info, CollectionPool>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<CreatePool>, collection_id: Pubkey, duration: i64) -> Result<()> {
+    let collection = &mut ctx.accounts.collection_pool;
+
+    collection.collection_id = collection_id;
+    collection.pool_owner = ctx.accounts.authority.key();
+    collection.duration = duration;
+    collection.total_offers = 0;
+    collection.bump = *ctx.bumps.get("collection_pool").unwrap();
+
+    Ok(())
+}
--- lend-borrow/programs/lend-borrow/src/lib.rs
+++ lend-borrow/programs/lend-borrow/src/lib.rs
@@ -1,5 +1,7 @@
+pub mod instructions;
 pub mod states;
 use anchor_lang::prelude::*;
+pub use instructions::*;
 pub use states::*;
 
 declare_id!("bpuPFSHXB4e8CAy1akbNH64XmBdBfzFH977x18Jng6f");
@@ -8,8 +10,12 @@ declare_id!("bpuPFSHXB4e8CAy1akbNH64XmBdBfzFH977x18Jng6f");
 pub mod lend_borrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn create_pool(
+        ctx: Context<CreatePool>,
+        collection_id: Pubkey,
+        duration: i64,
+    ) -> Result<()> {
+        instructions::create_pool::handler(ctx, collection_id, duration)
     }
 }
 
