9b00c46165840c22d227da46e7abba1ae6874967
Suvineet Singh <suvineet1998@gmail.com>
2024-03-02 14:23:19 +0000

    Implement trade handler - Extract and prepare Trade data - Determine Trade direction and price - Prepare and execute Trade - Notify the User

--- telegram-bot/dexterity-bot/src/api-utils/tradeHandler.ts
+++ telegram-bot/dexterity-bot/src/api-utils/tradeHandler.ts
@@ -6,19 +6,18 @@ export const tradeHandler = async (
   req: Request,
   trader: Trader,
   CopiedTrader: undefined | Trader,
-  AppState: Map<string, any>
+  AppState: Map<string, any>,
 ) => {
   if (req.method != "POST") {
     return new Response(
       JSON.stringify({ error: "Wrong method, this request is POST" }),
-      { status: 570 }
+      { status: 570 },
     );
   }
-
   if (!CopiedTrader) {
     return new Response(
       JSON.stringify({ error: "You have not set a trader to copy" }),
-      { status: 590 }
+      { status: 590 },
     );
   }
 
@@ -34,9 +33,94 @@ export const tradeHandler = async (
         maker: body.maker,
         taker: body.taker,
         copiedTrader: CopiedTrader.traderRiskGroup.toBase58(),
+        isThereCopiedTrader:
+          body.maker != CopiedTrader.traderRiskGroup.toBase58() &&
+          body.taker != CopiedTrader.traderRiskGroup.toBase58(),
       }),
-      { status: 590 }
+      { status: 590 },
     );
 
-    
+  // Extracting and Preparing Trade Data
+  const positioningRatio = AppState.get("positioningRatio");
+
+  const sizeFractional = dexterity.Fractional.New(
+    Math.floor((body.base_size * positioningRatio ?? 0) * 10),
+    1,
+  );
+
+  // Determining Trade Direction and Price
+  const isBid =
+    (body.taker_side === "ask") ===
+    (body.maker === CopiedTrader.traderRiskGroup.toBase58());
+
+  const maxSlippageAmount = body.price * 0.05;
+
+  const adjustedPrice = isBid
+    ? body.price + maxSlippageAmount
+    : body.price - maxSlippageAmount;
+
+  const priceFractional = dexterity.Fractional.New(
+    Math.floor(adjustedPrice * 1000),
+    3,
+  );
+
+  // Preparing for Trade Execution
+  await trader.connect(NaN, NaN);
+  trader.disconnect();
+
+  let productIndex;
+  for (const [name, { index }] of trader.getProducts()) {
+    if (!name.includes(body.product)) {
+      continue;
+    }
+    productIndex = index;
+    break;
+  }
+
+  // Executing the Trade
+  console.log({
+    productName: body.product,
+    productIndex,
+    isBid,
+    priceFractional: priceFractional.toDecimal(),
+    sizeFractional: sizeFractional.toDecimal(),
+    positioningRatio,
+  });
+
+  await trader.updateMarkPrices();
+  const signature = await trader.newOrder(
+    productIndex,
+    isBid,
+    priceFractional,
+    sizeFractional,
+    0,
+    null,
+    null,
+    null,
+    null,
+    {},
+  );
+
+  // Notifying the User
+  const tradeInfo = {
+    signature,
+    productName: body.product,
+    productIndex,
+    isBid,
+    priceFractional: priceFractional.toDecimal(),
+    sizeFractional: sizeFractional.toDecimal(),
+    positioningRatio,
+  };
+
+  sendTradeMessageToUser(tradeInfo);
+
+  return new Response(
+    JSON.stringify({
+      ok: "completed copy-trade",
+      tradeInfo,
+    }),
+    {
+      status: 200,
+    },
+  );
 };
